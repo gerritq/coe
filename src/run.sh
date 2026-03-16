e95e3a861f3b8fa12adc0622ddf98760a33bfd55
gerritq <g.quaremba@gmail.com>
2026-03-16 22:10:56 +0000

    need data cleaning updates

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=coe
+#SBATCH --job-name=coe_qwen32b
 #SBATCH --output=../logs/%j.out
 #SBATCH --error=../logs/%j.err
-#SBATCH --time=03:00:00
+#SBATCH --time=02:00:00
 #SBATCH --partition=gpu,nmes_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=15GB
@@ -12,13 +12,14 @@
 
 # Define your lists
 DATASETS=("wikipedia_chatgpt" "wikipedia_cohere" "wikipedia_bloomz" "arxiv_chatgpt" "arxiv_cohere" "arxiv_bloomz" "reddit_chatgpt" "reddit_cohere" "reddit_bloomz")
-MODELS=("qwen_06b" "qwen_8b" "llama_8b") # "qwen_32b"
+# MODELS=("qwen_06b" "qwen_8b" "llama_8b") # "qwen_32b"
+MODELS=("qwen_32b") # "qwen_32b"
 
-DATASETS=("wikipedia_chatgpt")
-MODELS=("qwen_06b") # "qwen_32b"
+# DATASETS=("wikipedia_chatgpt")
+# MODELS=("qwen_06b") # "qwen_32b"
 
 # Fixed parameters
-SMOKE_TEST=1
+SMOKE_TEST=0
 N=5000
 
 # Nested loop to run every model on every dataset
