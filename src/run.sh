0140bfdb466a6391e96b30b0fae0e7667f9a0b1a
gerritq <g.quaremba@gmail.com>
2026-03-19 11:43:18 +0000

    test with horizontal

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=coe_llama
+#SBATCH --job-name=coe_horizontal
 #SBATCH --output=../logs/%j.out
 #SBATCH --error=../logs/%j.err
-#SBATCH --time=02:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=gpu,nmes_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=15GB
@@ -20,15 +20,15 @@ nvidia-smi
 # MODELS=("qwen_06b" "qwen_8b" "llama_8b") # "qwen_32b"
 # MODELS=("qwen_32b") # "qwen_32b"
 
-DATASETS=("wikihow_chatgpt")
-MODELS=("qwen_06b") # "qwen_32b" "qwen_06b"
+DATASETS=("wikipedia_chatgpt")
+MODELS=("qwen_8b" "llama_8b") # "qwen_32b" "qwen_06b"
 
 # Fixed parameters
 MODES=("horizontal") # "last_token" "pooling" "horizontal"
-DIFF_VECTORS=(0)
+DIFF_VECTORS=(0 1)
 
 TEST=1
-SMOKE_TEST=1
+SMOKE_TEST=0
 N=10000
 
 # Nested loop to run every model on every dataset
