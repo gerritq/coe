2b48832d022d0d092928296d9b99f4c13edb03b1
gerritq <g.quaremba@gmail.com>
2026-03-29 15:10:16 +0100

    added ood steering projection plots

--- src/steering.sh
+++ src/steering.sh
@@ -14,7 +14,7 @@ nvidia-smi
 # DATASETS=("wikipedia_chatgpt" "arxiv_chatgpt")
 # MODELS=("llama_8b" "qwen_8b")
 
-DATASETS=("wikipedia_chatgpt" "arxiv_chatgpt" "wikihow_chatgpt")
+DATASETS=("wikipedia_chatgpt")
 MODELS=("llama_8b")  # "llama_8b"
 
 MODE="last_token"
@@ -24,7 +24,7 @@ N_VAL=-1
 N_TEST=-1
 PREFIX=0
 SMOKE_TEST=0
-OOD=0
+OOD=1
 
 for DATASET in "${DATASETS[@]}"; do
     for MODEL in "${MODELS[@]}"; do
