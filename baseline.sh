49990b0f3bee18a753051fba386343868524561f
gerritq <g.quaremba@gmail.com>
2026-05-05 06:25:50 +0100

    probing ood

--- baseline.sh
+++ baseline.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=b_ood_bi_fluo_bis
+#SBATCH --job-name=b_encoder
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
-#SBATCH --time=12:00:00
+#SBATCH --time=01:30:00
 #SBATCH --partition=gpu,nmes_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=50GB
-#SBATCH --constraint=h200|b200
-#SBATCH --exclude=erc-hpc-comp035,erc-hpc-comp050,erc-hpc-comp031
+#SBATCH --constraint=h200|b200|a100|l40s
+#SBATCH --exclude=erc-hpc-comp035,erc-hpc-comp050,erc-hpc-comp031,erc-hpc-comp038
 
 # set -euo pipefail
 
@@ -23,16 +23,16 @@ export CUDA_LAUNCH_BLOCKING=1
 # DATASETS=("tsm_paras_en" "tsm_paras_pt" "tsm_paras_vi" "tsm_sums_en" "tsm_sums_pt" "tsm_sums_vi")
 
 DATASETS=("detectrl_arxiv" "detectrl_writing_prompt" "detectrl_yelp_review" "detectrl_xsum" "multisocial_en" "multisocial_de" "multisocial_ru" "multisocial_zh" "multisocial_pt" "tsm_paras_en" "tsm_paras_pt" "tsm_paras_vi" "tsm_sums_en" "tsm_sums_pt" "tsm_sums_vi")
-
+# DATASETS=("detectrl_arxiv")
 
 SMOKE_TEST=0
-OOD=1
+OOD=0
 MODELS=(
         # "revise"
         # "gescore"
-        "biscope"
+        # "biscope"
         # "raidar"
-        "text_fluoroscopy"
+        # "text_fluoroscopy"
         # "radar"
         # "openai_roberta"
         # "repreguard"
