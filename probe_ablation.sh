81baac6ad74cc052c479b845d20759db4b19f007
gerritq <g.quaremba@gmail.com>
2026-05-12 16:27:49 +0100

    results

--- probe_ablation.sh
+++ probe_ablation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=probe_ablation_poly_c50
+#SBATCH --job-name=p_abl_mlp_depth_5_8
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
-#SBATCH --time=10:00:00
+#SBATCH --time=04:00:00
 #SBATCH --partition=gpu,nmes_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=50GB
@@ -18,16 +18,16 @@ cd "${ROOT_DIR}"
 MODELS=("llama_8b") # "llama_8b" "qwen_06b"
 
 # DATASETS=("drlDomain_arxiv" "tsm_first" "multisocial_en" "m4_gpt4")
-DATASETS=("tsm_extend" "tsm_first" "tsm_sums" "tsm_tst" "raidDomain_wiki")
-# Ablations
-# DATASETS=("raidDomain_wiki" "multisocial_en")
+DATASETS=("tsm_first" "tsm_extend" "tsm_sums" "tsm_tst")
+
+
 
 TOKEN_MODE="last_token"
 MODES=("mlp") # default | pca | meta | meta_attn | poly
 COMPONENTS_LIST=(50)
 TRAINING_SIZES=(-1)
 C_LIST=(1)
-MLP_DEPTH_LIST=(1 2 3 4 5)
+MLP_DEPTH_LIST=(5 6 7 8)
 FOLDER="ablation"
 SMOKE_TEST=0
 OOD=0
